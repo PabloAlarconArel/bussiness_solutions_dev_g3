7525c55415d730587e76821462a4290bf86fc5e1
Guillermo Gómez <forzakenone1@gmail.com>
2022-04-20 19:12:51 -0400

    Guillermo
    cambio en el nombre de una variable que estaba mal escrita y en el orden de otra variable

--- ejemplos/views.py
+++ ejemplos/views.py
@@ -1,7 +1,5 @@
-from django.shortcuts import render
 from rest_framework import generics, viewsets
-from rest_framework.decorators import (
-	api_view, authentication_classes, permission_classes)
+from rest_framework.decorators import (api_view, authentication_classes, permission_classes)
 from rest_framework.parsers import JSONParser
 from rest_framework.response import Response
 from rest_framework.views import APIView
