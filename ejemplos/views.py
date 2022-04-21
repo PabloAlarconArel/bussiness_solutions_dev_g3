73b8ccd1b021d8dadcea5ba565f4b26c4edf3e35
Guillermo Gómez <forzakenone1@gmail.com>
2022-04-20 20:25:57 -0400

    .
    .

--- ejemplos/views.py
+++ ejemplos/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import render
 from rest_framework import generics, viewsets
 from rest_framework.decorators import (api_view, authentication_classes, permission_classes)
 from rest_framework.parsers import JSONParser
