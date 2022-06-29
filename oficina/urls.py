16a69d5ab18e1bca6577d77e06707b242e1840d6
Israel <isrvelverdugo@gmail.com>
2022-06-28 22:31:42 -0400

    syo

--- oficina/urls.py
+++ oficina/urls.py
@@ -9,5 +9,11 @@ oficina_urlpatterns = [
      path('oficinas_oficina_list_rest/', views.oficinas_oficina_list_rest),
      path('oficinas_oficina_update_element_rest/', views.oficinas_oficina_update_element_rest),
      path('oficinas_oficina_get_element_rest/', views.oficinas_oficina_get_element_rest),
-     path('oficinas_oficina_del_element_rest/', views.oficinas_oficina_del_element_rest)
+     path('oficinas_oficina_del_element_rest/', views.oficinas_oficina_del_element_rest),
+
+    path('oficinas_main/', views.oficinas_main, name="oficinas_main"),
+    path('oficinas_oficina_add/', views.oficinas_oficina_add, name="oficinas_oficina_add"),
+    path('oficinas_oficina_save/', views.oficinas_oficina_save, name="oficinas_oficina_save"),
+    path('oficinas_oficina_ver/<oficina_id>/', views.oficinas_oficina_ver, name="oficinas_oficina_ver"),
+    path('oficinas_list_oficinas/', views.oficinas_list_oficinas, name="oficinas_list_oficinas"),
     ]
