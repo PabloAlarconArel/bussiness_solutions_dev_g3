4c843188eb098f6617a9b5b7ad6102aaa8d54950
Javitrox98 <javier_andres.98@live.cl>
2022-07-06 21:52:48 -0400

    Nueva App de sala

--- sala_new/models.py
+++ sala_new/models.py
@@ -5,14 +5,14 @@ from piso.models import Piso
 
 class Sala(models.Model):
     piso = models.ForeignKey(Piso,on_delete=CASCADE) 
-    nombre_sala_r = models.CharField(max_length=100, null=True, blank=True, verbose_name='Nombre sala de reunion')
-    capacidad_sala_r = models.IntegerField(null=True, default=0, blank=True, verbose_name='Capacidad')
+    nombre_sala = models.CharField(max_length=100, null=True, blank=True, verbose_name='Nombre sala de reunion')
+    capacidad_sala = models.IntegerField(null=True, default=0, blank=True, verbose_name='Capacidad')
     estado = models.CharField(max_length=100, null=True, blank=True, default='Activo', verbose_name='Estado')   
     created = models.DateTimeField(auto_now_add=True,verbose_name='Fecha Creación')
     updated = models.DateTimeField(auto_now=True,verbose_name='Fecha Actualización')
     class Meta:
         verbose_name = 'Sala'
         verbose_name_plural = 'Salas'
-        ordering = ['nombre_sala_r']   
+        ordering = ['nombre_sala']   
     def __str__(self):
-        return self.nombre_sala_r
+        return self.nombre_sala
